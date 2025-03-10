8263c9cae54ce6da694312350ec81d9dcd4c32a2
Harvey <harveydangel@gmail.com>
2025-03-10 17:29:35 +0800

    ddfegw

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
 	return (
 		<div className="flex justify-center w-full lg:max-h-[96px] top-0">
 			<header className="w-full lg:max-w-[1280px] h-full lg:p-[30px] flex lg:items-center lg:justify-between justify-center">
-				<h1 className="text-[40px] font-bold animate-pulse">HARBs</h1>
+				<h1 className="text-[40px] font-bold animate-pulse bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-500">HARBs</h1>
 				<div className="hidden text-[24px] lg:flex items-center lg:gap-[30px]">
 					<a href="#home" className="hover:text-indigo-500">
 						Home
