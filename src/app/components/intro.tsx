b17f50fc8ba3e81ff2a6a84eeb08099ea9f6a81a
Harvey <harveydangel@gmail.com>
2025-07-15 14:27:39 +0800

    Modify introduction

--- src/app/components/intro.tsx
+++ src/app/components/intro.tsx
@@ -13,7 +13,7 @@ export default function Intro() {
 					{`Hi! I'm `}<span className="text-[#D5FF4B]">{`Harvey Dangel`}</span>,
 					</h1>
 					<h1 className="text-[24px] text-center lg:text-left lg:text-[40px]">
-						{`an aspiring Software Engineer.`}
+						{`an aspiring Software Developer.`}
 					</h1>
 					<br />
 					<div className="flex gap-[30px] justify-center lg:justify-start">
